306e0824a53091bdfd56dc0519bfc08dc945ca88
Anant Chauhan <anantchauhangpn@gmail.com>
2022-08-05 19:31:49 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:appsite_store/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: HomePage(),
+
+    );
+  
+  }
+}
